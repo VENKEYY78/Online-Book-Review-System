d02362f59af5c8c46e9d38d1193ce595e3400236
VENKEYY78 <Jvenkeyy78@gmail.com>
2026-05-19 13:45:52 +0530

    Fix: correct full unique api backend baseURL endpoint layout

--- frontendfolder/src/components/services/api.jsx
+++ frontendfolder/src/components/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://vercel.app', 
+    baseURL: 'https://online-book-review-system.vercel.app', 
     withCredentials: true
 });
   
